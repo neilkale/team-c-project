4fdca2645742d87b701666391ea609c7533aea71
CarrotPeeler <83431976+CarrotPeeler@users.noreply.github.com>
2022-02-27 22:17:28 -0500

    Added button on dashboard that takes you to order tracker page with preloaded employee filter using logged in user

--- src/main/java/edu/wpi/cs3733/c22/teamC/controllers/windowControllers/DashboardController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamC/controllers/windowControllers/DashboardController.java
@@ -3,11 +3,15 @@ package edu.wpi.cs3733.c22.teamC.controllers.windowControllers;
 import edu.wpi.cs3733.c22.teamC.Databases.Employee;
 import edu.wpi.cs3733.c22.teamC.Databases.LoggedInUser;
 import edu.wpi.cs3733.c22.teamC.Databases.requests.ServiceRequest;
+import edu.wpi.cs3733.c22.teamC.controllers.AbstractController;
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.Pane;
 
-public class DashboardController {
+public class DashboardController extends AbstractController {
   @FXML ProgressIndicator progressCircle;
   @FXML Label notificationLabel;
   @FXML Label qtyLabel;
@@ -31,5 +35,21 @@ public class DashboardController {
     progressCircle.setProgress(percentOfRequestsComplete);
     qtyLabel.setText(complete + "/" + total);
     percentLabel.setText(Math.round(percentOfRequestsComplete * 100) + "%");
+  }
+
+  @FXML
+  private void trackOrdersButtonPressed() throws IOException {
+    // get logged in user
+    Employee currentUser = LoggedInUser.getCurrentUser();
+
+    // get controller of the order tracker page about to be loaded
+    FXMLLoader loader = getLoader("OrderTracker.fxml");
+    Pane root = loader.load();
+    controllerMediator.setOrderTrackerController(loader.getController());
+    controllerMediator.setDefaultPageCenter(root, "OrderTracker.fxml");
+
+    // preload filter in employee combobox in order tracker
+    controllerMediator.setOrderTrackerControllerAssigned(
+        currentUser.get_firstName() + " " + currentUser.get_lastName());
   }
 }

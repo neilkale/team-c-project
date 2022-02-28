ba3284b8006b9b2b4840a97b24c80f5021b2805b
ihmacinerney <ihmacinerney@wpi.edu>
2022-02-27 21:20:28 -0500

    Improved our icons again

--- src/test/java/edu/wpi/cs3733/c22/teamC/ControllersTest/ControllerTest.java
+++ src/test/java/edu/wpi/cs3733/c22/teamC/ControllersTest/ControllerTest.java
@@ -1,10 +1,12 @@
 package edu.wpi.cs3733.c22.teamC.ControllersTest;
-import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
 public class ControllerTest {
-    @Test
-    public void testFXMLConnection(){
-        assertEquals(true, false);
-    }
+  @Test
+  public void testFXMLConnection() {
+    assertEquals(true, false);
+  }
 }

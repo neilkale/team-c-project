36ad64da52aef3e3add61393c8cc6d2f06aeaf0d
“jack <81708456+JackBergin@users.noreply.github.com>
2022-02-28 20:17:38 -0500

    No dice at making any of the other teams api to work

--- src/main/java/edu/wpi/cs3733/c22/teamC/controllers/APIController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamC/controllers/APIController.java
@@ -1,9 +1,11 @@
 package edu.wpi.cs3733.c22.teamC.controllers;
 
 import com.jfoenix.controls.JFXButton;
+import edu.wpi.GoldenGandaberundas.Main;
 import edu.wpi.cs3733.c22.teamB.ServiceRequestAPI.BServiceRequestAPI;
 import edu.wpi.cs3733.c22.teamC.GiftServiceRequest;
-import edu.wpi.cs3733.c22.teamD.Main;
+import edu.wpi.fire_engine_foenix.Fapp;
+import edu.wpi.teame.TeamESecurityServiceRequest;
 import javafx.fxml.FXML;
 
 public class APIController extends AbstractController {
@@ -11,6 +13,9 @@ public class APIController extends AbstractController {
   @FXML private JFXButton TeamBAPI;
   @FXML private JFXButton TeamCAPI;
   @FXML private JFXButton TeamDAPI;
+  @FXML private JFXButton TeamEAPI;
+  @FXML private JFXButton TeamFAPI;
+  @FXML private JFXButton TeamGAPI;
 
   @FXML
   private void initialize() {}
@@ -26,15 +31,25 @@ public class APIController extends AbstractController {
   }
 
   @FXML
-  private void startTeamDAPI() {
-    runDAPI();
+  private void startTeamEAPI() {
+    runEAPI();
+  }
+
+  @FXML
+  private void startTeamFAPI() {
+    runFAPI();
+  }
+
+  @FXML
+  private void startTeamGAPI() {
+    runGAPI();
   }
 
   public void runBAPI() {
     BServiceRequestAPI computerAPI = BServiceRequestAPI.getInstance();
     try {
       // Platform.setImplicitExit(false);
-      computerAPI.run(0, 0, 1900, 1000, null, null, null);
+      computerAPI.run(0, 0, 800, 600, null, null, null);
     } catch (Exception e) {
       System.out.println("Failed to run API");
       e.printStackTrace();
@@ -53,12 +68,37 @@ public class APIController extends AbstractController {
     }
   }
 
-  public void runDAPI() {
-
-    Main securityAPI = new Main();
+  public void runEAPI() {
+    TeamESecurityServiceRequest securityAPI = TeamESecurityServiceRequest.getInstance();
     try {
       // Platform.setImplicitExit(false);
-      securityAPI.run(0, 0, 1900, 1000, null, null, null);
+      securityAPI.run(0, 0, 1900, 1000, null, null);
+
+    } catch (Exception e) {
+      System.out.println("Failed to run API");
+      e.printStackTrace();
+    }
+  }
+
+  public void runFAPI() {
+
+    Fapp medicineAPI = new Fapp();
+    try {
+      // Platform.setImplicitExit(false);
+      medicineAPI.run(0, 0, 1900, 1000, null, null, null);
+
+    } catch (Exception e) {
+      System.out.println("Failed to run API");
+      e.printStackTrace();
+    }
+  }
+
+  public void runGAPI() {
+
+    Main workflowAPI = new Main();
+    try {
+      // Platform.setImplicitExit(false);
+      workflowAPI.run(0, 0, 1900, 1000, null, 2);
 
     } catch (Exception e) {
       System.out.println("Failed to run API");

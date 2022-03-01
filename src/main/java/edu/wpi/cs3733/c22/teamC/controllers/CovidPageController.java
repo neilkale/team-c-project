2251d542e52f99922a7304423c58fb32e25506f0
Coco <73606549+cortinab@users.noreply.github.com>
2022-03-01 18:50:55 -0500

    Revert "Credits page coco"

--- src/main/java/edu/wpi/cs3733/c22/teamC/controllers/CovidPageController.java
+++
@@ -1,8 +0,0 @@
-package edu.wpi.cs3733.c22.teamC.controllers;
-
-import com.jfoenix.controls.JFXButton;
-import javafx.fxml.FXML;
-
-public class CovidPageController {
-  @FXML private JFXButton backButton;
-}
--- src/main/java/edu/wpi/cs3733/c22/teamC/controllers/windowControllers/SlideInfoMenuController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamC/controllers/windowControllers/SlideInfoMenuController.java
@@ -15,7 +15,7 @@ public class SlideInfoMenuController extends AbstractController {
 
   @FXML
   private void covidButtonPressed() throws IOException {
-    controllerMediator.setDefaultPageCenter("CovidPage.fxml");
+    // controllerMediator.setDefaultPageCenter(".fxml");
   }
 
   @FXML

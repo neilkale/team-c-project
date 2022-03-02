2562559449c166fe3542b3abcffaa8275a4b3858
Coco <cortinabarbieri@gmail.com>
2022-03-01 19:43:18 -0500

    fixes

--- src/main/java/edu/wpi/cs3733/c22/teamC/Databases/DaoPattern/EquipmentDaoImpl.java
+++ src/main/java/edu/wpi/cs3733/c22/teamC/Databases/DaoPattern/EquipmentDaoImpl.java
@@ -1,6 +1,6 @@
 package edu.wpi.cs3733.c22.teamC.Databases.DaoPattern;
 
-import edu.wpi.cs3733.c22.teamC.SQLMethods.requests.MedicalEquipmentQuery;
+import edu.wpi.cs3733.c22.teamC.SQLMethods.MedicalEquipmentQuery;
 
 public class EquipmentDaoImpl extends DaoInterface {
 

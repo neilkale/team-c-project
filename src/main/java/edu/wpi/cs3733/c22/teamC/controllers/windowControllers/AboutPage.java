2d8287c9fe0ac41c421c3533152eb40e4ad02d79
DilceO <44061575+DilceO@users.noreply.github.com>
2022-03-01 21:01:05 -0500

    controller?

--- src/main/java/edu/wpi/cs3733/c22/teamC/controllers/windowControllers/AboutPage.java
+++ src/main/java/edu/wpi/cs3733/c22/teamC/controllers/windowControllers/AboutPage.java
@@ -1,4 +1,11 @@
 package edu.wpi.cs3733.c22.teamC.controllers.windowControllers;
 
-public class AboutPage {
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.c22.teamC.controllers.AbstractController;
+import javafx.fxml.FXML;
+
+public class AboutPage extends AbstractController {
+    @FXML
+    private JFXButton backButton;
 }
+

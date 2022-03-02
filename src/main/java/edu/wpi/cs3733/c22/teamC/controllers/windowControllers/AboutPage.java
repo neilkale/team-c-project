2053214b87fe0bf71ac36c7cf7327124428ffd9f
DilceO <44061575+DilceO@users.noreply.github.com>
2022-03-01 21:04:08 -0500

    yuh yuh

--- src/main/java/edu/wpi/cs3733/c22/teamC/controllers/windowControllers/AboutPage.java
+++ src/main/java/edu/wpi/cs3733/c22/teamC/controllers/windowControllers/AboutPage.java
@@ -5,7 +5,5 @@ import edu.wpi.cs3733.c22.teamC.controllers.AbstractController;
 import javafx.fxml.FXML;
 
 public class AboutPage extends AbstractController {
-    @FXML
-    private JFXButton backButton;
+  @FXML private JFXButton backButton;
 }
-
